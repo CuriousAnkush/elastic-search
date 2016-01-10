cdebb084c6da501e40282663350c33595de6cee4
Ankush <ankush.maheshwari@payu.in>
2016-01-10 11:00:51 +0530

    Ankush: Added styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107173217) do
+ActiveRecord::Schema.define(version: 20151115153114) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "children", force: :cascade do |t|
+    t.integer  "parent_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "grades", force: :cascade do |t|
+    t.integer  "child_id",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "level",      limit: 255
+  end
+
+  create_table "parents", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
